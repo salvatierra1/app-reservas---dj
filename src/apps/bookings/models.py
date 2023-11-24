8428cc2a9bcc29fe5aee5c8e2c8fd2741d4582c0
salvatierra1 <victordanielsalvatierra6@gmail.com>
2023-11-24 20:28:09 -0300

    correction models

--- src/apps/bookings/models.py
+++ src/apps/bookings/models.py
@@ -1,14 +1,11 @@
 from django.db import models
-
 from apps.customers.models import Customers
 from apps.services.models import Services
 from apps.employees.models import Employees
 from apps.coordinators.models import Coordinators
 
-
 # Create your models here.
 
-<<<<<<< HEAD
 class Bookings(models.Model):
     booking_date = models.DateTimeField(auto_now_add=True)
     date = models.DateTimeField()
@@ -16,8 +13,4 @@ class Bookings(models.Model):
     service = models.ForeignKey(Services, on_delete=models.CASCADE)
     employee = models.ForeignKey(Employees, on_delete=models.CASCADE)
     coordinators = models.ForeignKey(Coordinators, on_delete=models.CASCADE)
-=======
 
-class Bookings(models.Model):
-    name = models.CharField(max_length=50)
->>>>>>> 353df4daa0fe43bd6a5823ddc9cd81c637d70669
--- src/apps/customers/models.py
+++ src/apps/customers/models.py
@@ -3,10 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Customers(models.Model):
-<<<<<<< HEAD
     name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=100)
     state = models.BooleanField(default=True)
-=======
-    name = models.CharField(max_length=50)
->>>>>>> 353df4daa0fe43bd6a5823ddc9cd81c637d70669
+
