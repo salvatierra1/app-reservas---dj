d2ab95d4b79b57401efe084dba2f178d354a1b9c
javierCh77 <javierchavarria21@gmail.com>
2023-11-28 19:33:42 -0300

    update bookings create and list

--- src/apps/bookings/urls.py
+++ src/apps/bookings/urls.py
@@ -6,4 +6,8 @@ app_name = 'apps.bookings'
 
 urlpatterns = [
      path('create/', views.BookingsCreateView.as_view(), name ='create'),
+     path('delete/', views.BookingsDeleteView.as_view(), name ='delete'),
+     path('list/', views.BookingsListView.as_view(), name ='list'),
+     path('update/<int:id>/', views.BookingsUpdateView.as_view(), name ='update'), 
+       
 ]
--- src/apps/bookings/views.py
+++ src/apps/bookings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Bookings
 from django.views import generic
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -9,3 +10,30 @@ class BookingsCreateView(generic.CreateView):
     model= Bookings
     fields= '__all__'
     template_name = 'bookings/create.html'
+    success_message = "¡La reserva '%(name)s' fue creado correctamente!"
+    success_url = reverse_lazy('apps.bookings:list') 
+     
+     
+class BookingsDeleteView(generic.DeleteView):
+    model = Bookings
+    success_url = reverse_lazy("bookings:listar")
+    template_name = 'bookings/delete.html'   
+    
+    
+class BookingsListView(generic.ListView):
+    model= Bookings
+    template_name = 'bookings/list.html'
+    context_object_name = 'bookings'   
+      
+      
+class BookingsUpdateView(generic.UpdateView):
+    model = Bookings
+    fields= '__all__'
+    template_name = 'bookings/update.html'
+    success_message = "¡La reserva '%(name)s' fue actualizado correctamente!"
+    success_url = reverse_lazy('apps.bookings:list')   
+    
+
+    
+ 
+    
